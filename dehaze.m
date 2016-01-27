3811bc774bb3675fe7046dd599056b7cf33d444d
CP <c.p.reinhardt@vu.nl>
2016-01-27 10:25:32 +0100

    changed dehaze

--- dehaze.m
+++ dehaze.m
@@ -1,4 +1,4 @@
-function [ radiance ] = dehaze( image, omega, win_size, lambda )
+function [radiance, transmission, trans_est, atmosphere, dark_channel] = dehaze( image, omega, win_size)
 %DEHZE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,11 +7,7 @@ if ~exist('omega', 'var')
 end
 
 if ~exist('win_size', 'var')
-    win_size = 15;
-end
-
-if ~exist('lambda', 'var')
-    lambda = 0.0001;
+    win_size = 1;
 end
 
 [m, n, ~] = size(image);
@@ -22,16 +18,9 @@ atmosphere = get_atmosphere(image, dark_channel);
 
 trans_est = get_transmission_estimate(image, atmosphere, omega, win_size);
 
-L = get_laplacian(image, zeros(size(image)));
-
-A = L + lambda * speye(size(L));
-b = lambda * trans_est(:);
-
-x = A \ b;
-
-transmission = reshape(x, m, n);
+transmission = imguidedfilter(trans_est,image,...
+    'NeighborhoodSize',[40 40],'DegreeOfSmoothing',10^(-2));
 
 radiance = get_radiance(image, transmission, atmosphere);
 
 end
-
